1bca5d577eab2c18c3fecf5aa02b0429f93faed9
FanBu <csubufan@gmail.com>
2017-11-30 11:01:31 -0500

    improve readibility of query 3

--- queries.sql
+++ queries.sql
@@ -7,17 +7,15 @@ WHERE Track.tid = PlayHistory.tid
 GROUP BY by_aid) AS playcounttable
 ON Artist.aid = playcounttable.by_aid;
 
-
 -- 3
-
-SELECT * FROM
-(SELECT COUNT(tid) AS n_J, by_aid
+SELECT Artist.aid, aname, description FROM
+(SELECT COUNT(tid) AS jazzcount, by_aid
 FROM Track
 WHERE genre = 'Jazz'
-GROUP BY by_aid) AS n_J_t,
-
-(SELECT COUNT(tid) AS n_T, by_aid
+GROUP BY by_aid) AS jazzcounttable,
+(SELECT COUNT(tid) AS totalcount, by_aid
 FROM Track
-GROUP BY by_aid) AS n_T_t
-
-WHERE n_T_t.n_T / n_J_t.n_J <= 2
+GROUP BY by_aid) AS totalcounttable, Artist
+WHERE jazzcounttable.by_aid = totalcounttable.by_aid
+AND jazzcounttable.by_aid = Artist.aid
+AND 2 * jazzcount >= totalcount;
