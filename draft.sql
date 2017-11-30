7f2dd0576cbf07283760912896cb42d0b4bfbbe9
bohanzhang <bz906@nyu.edu>
2017-11-29 22:54:19 -0500

    sample data

--- draft.sql
+++ draft.sql
@@ -33,18 +33,18 @@ CREATE TABLE `Follow` (
 
 
 
-INSERT INTO `follow` VALUES (1, 2);
-INSERT INTO `follow` VALUES (1, 3);
-INSERT INTO `follow` VALUES (1, 8);
-INSERT INTO `follow` VALUES (2, 3);
-INSERT INTO `follow` VALUES (2, 4);
-INSERT INTO `follow` VALUES (2, 5);
-INSERT INTO `follow` VALUES (3, 6);
-INSERT INTO `follow` VALUES (3, 7);
-INSERT INTO `follow` VALUES (4, 5);
-INSERT INTO `follow` VALUES (6, 7);
-INSERT INTO `follow` VALUES (7, 8);
-INSERT INTO `follow` VALUES (8, 1);
+INSERT INTO `follow` VALUES (1, 2, '2017-11-05 12:20:00');
+INSERT INTO `follow` VALUES (1, 3, '2016-10-01 17:40:00');
+INSERT INTO `follow` VALUES (1, 8, '2016-10-05 12:20:00');
+INSERT INTO `follow` VALUES (2, 3, '2016-05-11 18:10:00');
+INSERT INTO `follow` VALUES (2, 4, '2016-02-01 14:00:00');
+INSERT INTO `follow` VALUES (2, 5, '2017-01-23 12:20:00');
+INSERT INTO `follow` VALUES (3, 6, '2017-01-29 09:20:00');
+INSERT INTO `follow` VALUES (3, 7, '2017-09-10 14:20:00');
+INSERT INTO `follow` VALUES (4, 5, '2017-09-12 20:20:00');
+INSERT INTO `follow` VALUES (6, 7, '2017-09-15 21:50:00');
+INSERT INTO `follow` VALUES (7, 8, '2017-02-05 12:20:00');
+INSERT INTO `follow` VALUES (8, 1, '2017-11-05 02:20:00');
 
 
 DROP TABLE IF EXISTS `Track`;
@@ -57,11 +57,11 @@ CREATE TABLE `Track` (
   PRIMARY KEY (`tid`),
   FOREIGN KEY (`by_aid`) REFERENCES `Artist` (`aid`));
 
-INSERT INTO `Track` VALUES (1, 180000, 'Teriyaki Chicken', 'Jazz', 1);
-INSERT INTO `Track` VALUES (2, 200000, 'Crab Club', 'Blues', 1);
-INSERT INTO `Track` VALUES (3, 201000, 'Peking Track', 'Classical', 2);
-INSERT INTO `Track` VALUES (4, 190000, 'Indian Kitchen', 'Blues', 3);
-INSERT INTO `Track` VALUES (5, 170000, 'Sushi Bar', 'Jazz', 4);
+INSERT INTO `Track` VALUES (1, 180000, 'My Romance', 'Jazz', 1);
+INSERT INTO `Track` VALUES (2, 200000, 'Our Song', 'Blues', 1);
+INSERT INTO `Track` VALUES (3, 201000, 'Stolen Moments', 'Classical', 2);
+INSERT INTO `Track` VALUES (4, 190000, 'Virgo', 'Blues', 3);
+INSERT INTO `Track` VALUES (5, 170000, 'If You Could See Me Now', 'Jazz', 4);
 
 --duration: unifyied: miliseconds
 
@@ -82,13 +82,19 @@ INSERT INTO `Artist` VALUES (4, 'Bob Jones and Jones', 'joined by Jones');
 DROP TABLE IF EXISTS `AlbumPlaylist`;
 CREATE TABLE `AlbumPlaylist` (
   `alid` INT NOT NULL,
-  `title` INT NOT NULL,
+  `title` VARCHAR(200) NOT NULL,
   `time` TIMESTAMP NOT NULL,
   `type` ENUM('album', 'playlist'),
   `count` INT NOT NULL,
   `by_uid` INT,
   PRIMARY KEY (`alid`),
   FOREIGN KEY (`by_uid`) REFERENCES `User` (`uid`));
+
+
+INSERT INTO `AlbumPlaylist` VALUES (1, 'Songs of Rod', '2015-11-05 12:20:00','playlist',2,1);
+INSERT INTO `AlbumPlaylist` VALUES (2, 'Punk Band', '2016-01-01 12:00:00','album',2000,null);
+INSERT INTO `AlbumPlaylist` VALUES (3, 'Love Early', '2011-11-11 11:11:00','album',10000,null);
+
 
 
 DROP TABLE IF EXISTS `Rating`;
@@ -101,6 +107,16 @@ CREATE TABLE `Rating` (
   FOREIGN KEY (`uid`) REFERENCES `User` (`uid`),
   FOREIGN KEY (`tid`) REFERENCES `Track` (`tid`));
 
+
+INSERT INTO `Rating` VALUES (1, 1, '2015-11-05 12:20:00', 4);
+INSERT INTO `Rating` VALUES (1, 2, '2016-01-01 12:00:00', 5);
+INSERT INTO `Rating` VALUES (1, 5, '2011-11-11 11:11:00', 1);
+INSERT INTO `Rating` VALUES (2, 3, '2015-11-05 12:20:00', 3);
+INSERT INTO `Rating` VALUES (3, 3, '2016-01-01 12:00:00', 4);
+INSERT INTO `Rating` VALUES (4, 4, '2011-11-11 11:11:00', 4);
+
+
+
 DROP TABLE IF EXISTS `Like`;
 CREATE TABLE `Like` (
   `uid` INT NOT NULL,
@@ -109,6 +125,12 @@ CREATE TABLE `Like` (
   PRIMARY KEY (`uid`,`time`),  
   FOREIGN KEY (`uid`) REFERENCES `User` (`uid`),
   FOREIGN KEY (`aid`) REFERENCES `Artist` (`aid`));
+
+INSERT INTO `Like` VALUES (2, 1, '2015-11-05 12:20:00');
+INSERT INTO `Like` VALUES (2, 2, '2016-01-01 12:00:00');
+INSERT INTO `Like` VALUES (3, 3, '2011-11-11 11:11:00');
+INSERT INTO `Like` VALUES (7, 4, '2015-04-05 12:20:00');
+-------------------------
 
 
 DROP TABLE IF EXISTS `PlayHistory`;
@@ -122,6 +144,13 @@ CREATE TABLE `PlayHistory` (
   FOREIGN KEY (`tid`) REFERENCES `Track` (`tid`),
   FOREIGN KEY (`alid`) REFERENCES `AlbumPlaylist` (`alid`));
 
+INSERT INTO `PlayHistory` VALUES (3, '2015-11-05 12:20:00',4,2);
+INSERT INTO `PlayHistory` VALUES (3, '2016-01-01 12:00:00',5,2);
+INSERT INTO `PlayHistory` VALUES (6, '2011-11-11 11:11:00',3,1);
+INSERT INTO `PlayHistory` VALUES (8, '2015-04-05 12:20:00',1,3);
+
+
+
 DROP TABLE IF EXISTS `Trackalbum`;
 CREATE TABLE `Trackalbum` (
   `tid` INT NOT NULL,
@@ -131,11 +160,11 @@ CREATE TABLE `Trackalbum` (
   FOREIGN KEY (`alid`) REFERENCES `AlbumPlaylist` (`alid`),
   FOREIGN KEY (`tid`) REFERENCES `Track` (`tid`));
 
+INSERT INTO `Trackalbum` VALUES (1, 1, 1);
+INSERT INTO `Trackalbum` VALUES (2, 1, 2);
+INSERT INTO `Trackalbum` VALUES (3, 1, 3);
+INSERT INTO `Trackalbum` VALUES (4, 2, 1);
 
-INSERT INTO `album` VALUES (1, 'Rod liked songs', '2017-11-05', 1);
-INSERT INTO `album` VALUES (2, 'Crab People', '2005-12-01', 1);
-INSERT INTO `album` VALUES (3, 'Peking Peking', '2011-07-12', 2);
-INSERT INTO `album` VALUES (4, 'Hell Kitchen', '2008-09-05', 3);
-INSERT INTO `album` VALUES (5, 'Sushi Feast', '2001-01-03', 4);
+
 
 
