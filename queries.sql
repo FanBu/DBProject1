3976c2aaf6b543b762af30002ffc5665ed06383d
bohanzhang <bz906@nyu.edu>
2017-11-30 19:03:25 -0500

    shit work

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
+-- 1
+SET autocommit = 0;
+INSERT INTO User VALUES (NULL,'jeremyBo','jbj',' ','password',' ');
+-- does the question mean only 3 are required?
+ROLLBACK;
+
 -- 2
 SELECT aid, aname, (playcounttable.playcount) AS count 
 FROM Artist LEFT JOIN
@@ -20,6 +26,12 @@ WHERE jazzcounttable.by_aid = totalcounttable.by_aid
 AND jazzcounttable.by_aid = Artist.aid
 AND 2 * jazzcount >= totalcount;
 
+-- 4
+SET autocommit = 0;
+INSERT INTO `Rating` VALUES (4, 4, '2017-11-30 12:50:00', 5);
+
+ROLLBACK;
+
 -- 5
 SELECT alid, title, time, type, count, by_uid FROM AlbumPlaylist,
 (SELECT f_uid FROM Follow, User
@@ -27,3 +39,70 @@ WHERE Follow.uid = User.uid
 AND uname = 'NancyInQueens') AS uidtable
 WHERE AlbumPlaylist.type = 'playlist'
 AND AlbumPlaylist.by_uid = uidtable.f_uid;
+
+-- 6
+-- Better use CONTAINS? returns ranks using AND, NEAR, ACCUM
+-- But it's a SQL thing
+SELECT tid FROM Track
+WHERE CONTAINS(Track.title,'"Our" OR "Virgo" OR "Stolen"')
+
+
+
+-- what does it mean by some set?
+-- match at least of one element in a set or all elements in a set
+
+SELECT Track_table.title 
+FROM
+	(SELECT * 
+	FROM Track,
+		(SELECT aid 
+		FROM Artist
+		WHERE aname LIKE '%c%'
+		OR aname LIKE '%l%') 
+		AS aid_table
+	WHERE aid_table.aid = Track.by_aid) 
+	AS A_T_table
+
+	full join 
+
+	(SELECT * 
+	FROM Track
+	WHERE title LIKE '%c%'
+	OR title LIKE '%l%') 
+	AS Track_table
+	
+	ON A_T_table.tid = Track_table.tid
+
+-- 7
+SELECT aid FROM
+
+(SELECT uid 
+FROM Likes
+GROUP BY aid) AS table1,
+
+(SELECT uid 
+FROM Likes
+GROUP BY aid) AS table2
+
+WHERE table1.uid = table2.uid 
+
+-- this is total number of Fans
+(SELECT COUNT(uid) 
+FROM Likes 
+GROUP BY aid) n_uid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
