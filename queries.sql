456dc47f53a5c98dbdcbedc9d44e7fd9f0694b75
FanBu <csubufan@gmail.com>
2017-11-30 19:02:34 -0500

    query 7

--- queries.sql
+++ queries.sql
@@ -27,3 +27,28 @@ WHERE Follow.uid = User.uid
 AND uname = 'NancyInQueens') AS uidtable
 WHERE AlbumPlaylist.type = 'playlist'
 AND AlbumPlaylist.by_uid = uidtable.f_uid;
+
+-- 7
+SELECT counttotaltable.aid AS aid1, commontable.aid2
+FROM (SELECT aid, COUNT(uid) AS countuid FROM Likes GROUP BY aid) AS counttotaltable,
+(SELECT likes1.aid AS aid1, likes2.aid AS aid2, COUNT(likes1.uid) AS countcommon FROM 
+(SELECT aid, uid FROM Likes) AS likes1,
+(SELECT aid, uid FROM Likes) AS likes2
+WHERE likes1.uid = likes2.uid
+GROUP BY likes1.aid, likes2.aid) AS commontable
+WHERE NOT counttotaltable.aid = commontable.aid2
+AND counttotaltable.aid = commontable.aid1
+AND counttotaltable.countuid <= commontable.countcommon * 10;
+
+-- check similarity
+SELECT counttotaltable.aid AS aid1, commontable.aid2,
+commontable.countcommon / counttotaltable.countuid AS similarity
+FROM (SELECT aid, COUNT(uid) AS countuid FROM Likes GROUP BY aid) AS counttotaltable,
+(SELECT likes1.aid AS aid1, likes2.aid AS aid2, COUNT(likes1.uid) AS countcommon FROM 
+(SELECT aid, uid FROM Likes) AS likes1,
+(SELECT aid, uid FROM Likes) AS likes2
+WHERE likes1.uid = likes2.uid
+GROUP BY likes1.aid, likes2.aid) AS commontable
+WHERE NOT counttotaltable.aid = commontable.aid2
+AND counttotaltable.aid = commontable.aid1
+ORDER BY similarity DESC;
